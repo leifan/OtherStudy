f525d10ab08272a3ad441899dc73f7039e7065bd
leifan <271995806@qq.com>
2019-02-28 00:03:09 +0800

    拼接图

--- python/截图/t1.py
+++ python/截图/t1.py
@@ -1,14 +1,68 @@
 # -*- coding: UTF-8 -*-
-from PIL import ImageGrab
 
+import os
+from PIL import ImageGrab
+from PIL import Image
 
 # 参数说明
 # 第一个参数 开始截图的x坐标
 # 第二个参数 开始截图的y坐标
 # 第三个参数 结束截图的x坐标
 # 第四个参数 结束截图的y坐标
-bbox = (0, 0, 1160, 1080)
+bbox = (0, 0, 500, 500)
 im = ImageGrab.grab(bbox)
-
 # 参数 保存截图文件的路径
-im.save('as.png')
+im.save('1.jpeg')
+
+bbox = (500, 500, 1000, 1000)
+im = ImageGrab.grab(bbox)
+# 参数 保存截图文件的路径
+im.save('2.jpeg')
+
+#图片压缩后的大小
+width_i = 1000
+height_i = 1000
+
+#每行每列显示图片数量
+line_max = 3
+row_max = 3
+
+#参数初始化
+all_path = []
+num = 0
+pic_max=line_max*row_max
+
+dirName = os.getcwd()
+
+for root, dirs, files in os.walk(dirName):
+        for file in files:
+            if "jpeg" in file:
+                    all_path.append(os.path.join(root, file))
+
+toImage = Image.new('RGBA',(width_i*line_max,height_i*row_max))
+
+
+for i in range(0,row_max): 
+    for j in range(0,line_max):
+        pic_fole_head =  Image.open(all_path[num])
+        width,height =  pic_fole_head.size
+        print(i,j,width,height)
+    
+        tmppic = pic_fole_head.resize((width_i,height_i))
+        print(tmppic)
+        loc = (int((i%line_max)*width_i),int((j%line_max)*height_i))
+        print("第" + str(num) + "存放位置" + str(loc))
+        toImage.paste(tmppic,loc)
+        num= num + 1
+
+        if num >= len(all_path):
+                print(num, " break")
+                break
+
+    if num >= pic_max or num >= len(all_path):
+        print(num, "-- break")
+        break
+
+
+print(toImage.size)
+toImage.save('merged.png')
--- python/控制窗体/1.控制窗体.py
+++ python/控制窗体/1.控制窗体.py
@@ -4,20 +4,41 @@
 
 import win32con
 import win32gui
+import win32api
 import time
 
 # 以qq为例子
 qqWin = win32gui.FindWindow("TXGuifoundation", "QQ")  # 接收两个参数，一个是程序的类，一个是标题.目的是找到窗体
+print(qqWin)
 
+# 获得窗口的菜单句柄
+menuHandle = win32gui.GetMenu(qqWin)
+print(menuHandle)
+# 获得子菜单或下拉菜单句柄   
+# 参数：菜单句柄 子菜单索引号
+subMenuHandle = win32gui.GetSubMenu(menuHandle, 0)
+print(subMenuHandle)
 
+# # 搜索子窗口
+# # 枚举子窗口
+# hwndChildList = []     
+# win32gui.EnumChildWindows(qqWin, lambda hwnd, param: param.append(hwnd),  hwndChildList)
+# print(hwndChildList)
 
 while True:
-    # 然后让窗体显示出来
-    win32gui.ShowWindow(qqWin, win32con.SW_SHOW)
+    # # 然后让窗体显示出来
+    # win32gui.ShowWindow(qqWin, win32con.SW_SHOW)
 
-    time.sleep(2)
+    # time.sleep(2)
 
-    # 也可以让窗体消失
-    win32gui.ShowWindow(qqWin, win32con.SW_HIDE)
+    # # 也可以让窗体消失
+    # win32gui.ShowWindow(qqWin, win32con.SW_HIDE)
 
-    time.sleep(2)
+    time.sleep(3)
+    print(win32api.GetCursorPos())
+    # 通过坐标获取窗口句柄
+    # hw = win32api.WindowFromPoint(win32api.GetCursorPos())
+    # print(hw)
+    # print(win32gui.GetClassName(hw))
+    # print(win32gui.GetWindowText(hw))
+    # print(win32gui.GetwindowRect(hw))
