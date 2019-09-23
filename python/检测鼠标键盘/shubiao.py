0119679d1bf325f92e0d5c43ffcb9cbfe68588d6
leifan <271995806@qq.com>
2019-09-23 22:57:20 +0800

    嵌入式Linux-01

--- python/检测鼠标键盘/shubiao.py
+++ python/检测鼠标键盘/shubiao.py
@@ -1,6 +1,30 @@
 
+import os
+from PIL import ImageGrab
+from PIL import Image
+import time
+import datetime
 
 from pynput import keyboard,mouse
+
+pointXY = [0,0,0,0]
+pointXY_tmp = [0,0]
+
+def saveImg(x_s,y_s,x_e,y_e):
+    # 参数说明
+    # 第一个参数 开始截图的x坐标
+    # 第二个参数 开始截图的y坐标
+    # 第三个参数 结束截图的x坐标
+    # 第四个参数 结束截图的y坐标
+    bbox = (x_s,y_s,x_e,y_e)
+    im = ImageGrab.grab(bbox)
+    # 参数 保存截图文件的路径
+    signtime = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:17]
+    #time.strftime("%Y%m%d%H%M%S", time.localtime())
+
+    pathimgname = os.getcwd() + '\\output\\' + str(signtime + '.png')
+    im.save(pathimgname)
+    print('save img:', pathimgname)
 
 def on_press(key):
     try:
@@ -13,6 +37,13 @@ def on_press(key):
 def on_release(key):
     print('{0} released'.format(
         key))
+    if key == keyboard.Key.alt_l:
+        pointXY[0:2] = pointXY_tmp[0:2]
+        print('pointXY:',pointXY)
+    if key == keyboard.Key.alt_r:
+        pointXY[2:4] = pointXY_tmp[0:2]
+        print('pointXY:',pointXY)
+        saveImg(pointXY[0],pointXY[1],pointXY[2],pointXY[3])
     if key == keyboard.Key.esc:
         # Stop listener
         return False
@@ -20,6 +51,11 @@ def on_release(key):
 def on_move(x, y):
     print('Pointer moved to {0}'.format(
         (x, y)))
+    pointXY_tmp[0] = x
+    pointXY_tmp[1] = y
+    print('pointXY_tmp:',pointXY_tmp)
+
+    
 
 def on_click(x, y, button, pressed):
     print('{0} at {1}'.format(
@@ -31,7 +67,7 @@ def on_click(x, y, button, pressed):
 
 def on_scroll(x, y, dx, dy):
     print('Scrolled {0} at {1}'.format(
-        'down' if dy < 0 else 'up',
+        'down' if dy < 0 else 'up', 
         (x, y)))
 
 # Collect events until released
